b8d6a466c2a9f3600c624489f9265246e31915c1
BryanQuintanaTV <78245905+BryanQuintanaTV@users.noreply.github.com>
2022-08-25 09:13:51 -0600

    Evaluación 1 Practica 6 "Nano Time"
    25/08/2022

--- EVA1_6_NANO_TIME/src/eva1_6_nano_time/EVA1_6_NANO_TIME.java
+++ EVA1_6_NANO_TIME/src/eva1_6_nano_time/EVA1_6_NANO_TIME.java
@@ -1,17 +1,20 @@
 /*
- * "
- * "
+ * "Evaluación 1 Practica 6 "Nano Time"
+ *  25-25 / 08 / 2022"
  */
 package eva1_6_nano_time;
 
 /**
- *
  * @author Quintana Juarez Bryan Alexis
  */
 public class EVA1_6_NANO_TIME {
 
     public static void main(String[] args) {
-        System.out.println(System.nanoTime());
+        long ini = System.nanoTime();
+        System.out.println(ini);
+        long fin = System.nanoTime();
+        long tiempo = fin - ini;
+        System.out.println(tiempo);
     }
     
 }
